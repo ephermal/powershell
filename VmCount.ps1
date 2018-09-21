3f4c689025a327f4bad41a6bc320ae448bf333ec
Gerald Reid <gerald.reid@gmail.com>
2018-09-21 16:19:39 -0500

    initial

---
+++ VmCount.ps1
@@ -0,0 +1,14 @@
+
+$VMhosts = Get-Content c:\Users\Administrator\Desktop\esxilist.txt
+$Vmcount = @()
+
+Get-Content c:\Users\Administrator\Desktop\esxilist.txt | ForEach-Object{
+    write-host $_
+    Connect-ViServer $_ -Username root -Password  PASSWORD
+    Get-VMHost | Select @{N="Cluster";E={Get-Cluster -VMHost $_}},Name,@{N="NumVM";E={($_ |Get-VM | where {$_.PowerState -eq "PoweredOn"}).Count}}
+    $Vmcount = $Vmcount + (Get-VMHost | Select @{N="NumVM";E={($_ |Get-VM | where {$_.PowerState -eq "PoweredOn"}).Count}})
+    
+    }
+echo $VmCount
+
+
